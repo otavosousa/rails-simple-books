1f60257c960eb455a1d666da46dbd502ee87260a
Otavio Sousa <otavioklearskin@gmail.com>
2022-06-14 21:58:00 -0300

    create scaffold book genre

---
+++ app/views/genres/_genre.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! genre, :id, :description, :created_at, :updated_at
+json.url genre_url(genre, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_15_000803) do
+ActiveRecord::Schema.define(version: 2022_06_15_005005) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name_author"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 2022_06_15_000803) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "author_id"
     t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "books", "authors"
